07cd91eef341b7d2c74245924b926241e2b7245d
Peter Strizhev <strizhevp@gmail.com>
2024-10-18 02:14:49 -0700

    Initial commit

---
+++ GPTChatAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPTChatAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GPTChatAdventure/assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTChatAdventure.assets
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; } = 100;
+        // Stretch to add inventory, just an array of items
+        
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public void ShowStatus()
+        {
+            Console.WriteLine($"Player: {Name}");
+            Console.WriteLine($"Health: {Health}");
+            // Display inventory eventually
+        }
+    }
+}
